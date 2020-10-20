5685ac9f06a3e30859ec8b4f0539b77e2edb898a
Omega <muzurowa.lidiya@yandex.ru>
2020-10-20 13:56:58 +0300

    pattern "Singleton"

--- text-builder/src/text-builder/singleton/text-singleton.ts
+++ text-builder/src/text-builder/singleton/text-singleton.ts
@@ -1,6 +1,15 @@
-import {Injectable} from "@nestjs/common";
+export class TextSingleton {
+    private static instance: TextSingleton;
 
-@Injectable()
-export class TextSingleton{
+    private constructor() {
+    }
 
+    public static getInstance(): TextSingleton {
+        if (!TextSingleton.instance) {
+            TextSingleton.instance = new TextSingleton();
+        }
+        return TextSingleton.instance;
+    }
 }
+
+const instance = TextSingleton.getInstance();
